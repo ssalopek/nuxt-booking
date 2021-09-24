b3fbfd44b21312736880da6dc0f7095e8a0cd166
Stjepan <ssalopek.hr@gmail.com>
2021-09-24 15:42:16 +0200

    Error page

---
+++ layouts/error.vue
@@ -0,0 +1,31 @@
+<template>
+  <div style="text-align: center">
+    <img
+      style="
+        margin-top: 16px;
+        margin-bottom: 32px;
+        weight: 720px;
+        height: 360px;
+      "
+      src="https://www.argildx.com/wp-content/uploads/2017/06/Is-a-Misconfigured-%E2%80%9CPage-Not-Found%E2%80%9D-Jeopardizing-Your-Website.jpg"
+    />
+    <br />
+    {{ error.message }} {{ error.statusCode }}
+  </div>
+</template>
+
+<script>
+export default {
+  head() {
+    return {
+      title: "ERROR",
+    };
+  },
+  props: {
+    error: {
+      type: Object,
+      required: true,
+    },
+  },
+};
+</script>
--- plugins/dataApi.js
+++ plugins/dataApi.js
@@ -1,20 +1,43 @@
 export default function(context, inject) {
   const appId = "IYLSKHWLS7";
   const apikey = "3213e09fa4947abaa9ead95dde3753cf";
+  const headers = {
+    "X-Algolia-API-Key": apikey,
+    "X-Algolia-Application-Id": appId,
+  };
 
   inject("dataApi", { getHome });
 
   async function getHome(homeId) {
-    const response = await fetch(
-      `https://${appId}-dsn.algolia.net/1/indexes/homes/${homeId}`,
-      {
-        headers: {
-          "X-Algolia-API-Key": apikey,
-          "X-Algolia-Application-Id": appId,
-        },
-      }
-    );
+    try {
+      return unWrap(
+        await fetch(
+          `https://${appId}-dsn.algolia.net/1/indexes/homes/${homeId}`,
+          { headers }
+        )
+      );
+    } catch (error) {
+      return getErrorResponse(error);
+    }
+  }
+
+  async function unWrap(response) {
     const json = await response.json();
-    return json;
+    const { ok, status, statusText } = response;
+    return {
+      json,
+      ok,
+      status,
+      statusText,
+    };
+  }
+
+  function getErrorResponse(error) {
+    return {
+      ok: false,
+      status: 500,
+      statusText: error.message,
+      json: {},
+    };
   }
 }
