24fcedf1620074b2e96757e13eb694c176b50ac4
Stjepan <ssalopek.hr@gmail.com>
2021-09-25 16:32:24 +0200

    Style search page props

--- pages/search.vue
+++ pages/search.vue
@@ -1,21 +1,25 @@
 <template>
-  <div>
-    Results for {{ label }}<br />
-    <div style="height: 800px; width: 800px; float: right" ref="map"></div>
-    <div v-if="homes.length > 0">
-      <nuxt-link
-        v-for="home in homes"
-        :key="home.objectID"
-        :to="`/home/${home.objectID}`"
-      >
-        <HomeRow
-          :home="home"
-          @mouseover.native="highlightMarker(home.objectID, true)"
-          @mouseout.native="highlightMarker(home.objectID, flase)"
-        />
-      </nuxt-link>
+  <div class="app-search-results-page">
+    <div class="app-search-results">
+      <div class="app-search-results-listing">
+        <h2 class="app-title">Stays in {{ label }}</h2>
+        <nuxt-link
+          v-for="home in homes"
+          :key="home.objectID"
+          :to="`/home/${home.objectID}`"
+        >
+          <HomeRow
+            class="app-house"
+            :home="home"
+            @mouseover.native="highlightMarker(home.objectID, true)"
+            @mouseout.native="highlightMarker(home.objectID, flase)"
+          />
+        </nuxt-link>
+      </div>
+      <div class="app-search-results-map">
+        <div class="app-map" ref="map"></div>
+      </div>
     </div>
-    <div v-else>No results found</div>
   </div>
 </template>
 
@@ -48,6 +52,7 @@ export default {
     },
 
     getHomeMarkers() {
+      if (this.homes.length == 0) return NULL;
       return this.homes.map((home) => {
         return {
           ...home._geoloc,
